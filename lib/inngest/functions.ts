b47c725792d1b671978dbe74458257f6b81ef1d6
theBappy <abappy575@gmail.com>
2025-10-19 15:21:29 +0600

    cron for once a month not getting to much email in my inbox

--- lib/inngest/functions.ts
+++ lib/inngest/functions.ts
@@ -61,7 +61,7 @@ export const sendSignUpEmail = inngest.createFunction(
 
 export const sendDailyNewsSummary = inngest.createFunction(
   { id: "daily-news-summary" },
-  [{ event: "app/send.daily.news" }, { cron: "0 12 * * *" }],
+  [{ event: "app/send.daily.news" }, { cron: "0 12 L * *" }],
   async ({ step }) => {
     // Step #1: Get all users for news delivery
     const users = await step.run("get-all-users", getAllUsersForNewsEmail);
