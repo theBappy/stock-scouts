736b7b5a4fc6508f48f32c11182fcc838c73c9c6
theBappy <abappy575@gmail.com>
2025-10-19 14:45:05 +0600

    Add SearchCommand component and useDebounce hook for stock search functionality

--- app/(root)/_components/nav-items.tsx
+++ app/(root)/_components/nav-items.tsx
@@ -3,6 +3,7 @@
 import { NAV_ITEMS } from "@/lib/constants";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import SearchCommand from "./search/search- command";
 
 const NavItems = () => {
   const pathname = usePathname();
@@ -12,18 +13,31 @@ const NavItems = () => {
   };
   return (
     <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
-      {NAV_ITEMS.map(({ href, label }) => (
-        <li className="" key={href}>
-          <Link
-            className={`hover:text-yellow-500 transition-colors ${
-              isActive(href) ? "text-gray-100" : ""
-            }`}
-            href={href}
-          >
-            {label}
-          </Link>
-        </li>
-      ))}
+      {NAV_ITEMS.map(({ href, label }) => {
+        if (label === "Search")
+          return (
+            <li key="search-trigger">
+              <SearchCommand
+                renderAs="text"
+                label="Search"
+                initialStocks={[]}
+              />
+            </li>
+          );
+
+        return (
+          <li className="" key={href}>
+            <Link
+              className={`hover:text-yellow-500 transition-colors ${
+                isActive(href) ? "text-gray-100" : ""
+              }`}
+              href={href}
+            >
+              {label}
+            </Link>
+          </li>
+        );
+      })}
     </ul>
   );
 };
